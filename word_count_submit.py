69c86d654e29d64843da82d1e1b50583901f3c8d
Suhong Li <sli@bryant.edu>
2023-09-11 12:22:42 -0400

    chapter 2-3 and asgn1

---
+++ word_count_submit.py
@@ -0,0 +1,29 @@
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+
+# change the account name to your email account
+account='sli'
+
+# define a root path to access the data in the DataAnalysisWithPythonAndPySpark
+root_path='/net/clusterhn/home/'+account+'/isa540/Data/'
+
+spark = SparkSession.builder.appName(
+    "Counting word occurences from a book."
+).getOrCreate()
+
+spark.sparkContext.setLogLevel("WARN")
+
+# If you need to read multiple text files, replace `1342-0` by `*`.
+results = (
+    spark.read.text(root_path+"/gutenberg_books/1342-0.txt")
+    .select(F.split(F.col("value"), " ").alias("line"))
+    .select(F.explode(F.col("line")).alias("word"))
+    .select(F.lower(F.col("word")).alias("word"))
+    .select(F.regexp_extract(F.col("word"), "[a-z']*", 0).alias("word"))
+    .where(F.col("word") != "")
+    .groupby(F.col("word"))
+    .count()
+)
+
+results.orderBy("count", ascending=False).show(10)
+results.coalesce(1).write.csv("results_single_partition.csv")
